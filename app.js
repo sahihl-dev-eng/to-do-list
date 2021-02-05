742abda3442bc84e558aeabbd1e13aa6ff09e057
sahil <ersahilgalundia17@gmail.com>
2021-02-05 22:03:46 +0530

    added procfile gitignore

--- app.js
+++ app.js
@@ -136,6 +136,10 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port=process.env.Port;
+if(port==null || port=""){
+  port=3000;
+}
+app.listen(port, function() {
+  console.log("Server started succcesfully");
 });
